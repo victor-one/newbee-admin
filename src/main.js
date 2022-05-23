fd926cc6f26c89940333e54b3562144b03f11cc4
victor-one <513044829@qq.com>
2022-05-23 16:37:50 +0800

    管理后台左右栏目布局

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ElAside, ElButton, ElContainer, ElMenu, ElMenuItem, ElMenuItemGroup, ElSubmenu } from 'element-plus'
+import router from '@/router'
 
-createApp(App).mount('#app')
+import '~/theme/index.css';
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(ElButton).use(ElContainer).use(ElAside).use(ElMenu).use(ElMenuItem).use(ElMenuItemGroup).use(ElSubmenu)
+
+app.mount('#app')
---
+++ src/views/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><el-button type="primary">主要按钮</el-button></div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
